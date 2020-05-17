2631f16b36077b94191ac5b0127c7f8f8ddbed4b
Vicente <vicente.palomero.lopez@gmail.com>
2020-05-17 18:47:15 +0200

    Add branch in bashrc

--- global_setup.sh
+++ global_setup.sh
@@ -37,12 +37,25 @@ if [[ -z $alias ]] || [[ $alias == "Y" ]]; then
     $(git config --global alias.cleanup "!git branch --merged | egrep -v \"(^\*|master|dev)\" | xargs git branch -d")
 fi;
 
+read -p "Do you want to add the branch you are working on into your .bashrc file? [Y/n]: " prompt
+if [[ -z $prompt ]] || [[ $prompt == "Y" ]]; then
+    $(echo "" >> ~/.bashrc)
+    $(echo "# Add git branch in prompt (from git-tools)" >> ~/.bashrc)
+    $(echo "parse_git_branch() { git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/' ; }" >> ~/.bashrc)
+    $(echo 'export PS1="\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\[\033[33m\]\$(parse_git_branch)\[\033[00m\] $ "' >> ~/.bashrc)
+    $(echo "# End Add git branch in prompt from git-tools)" >> ~/.bashrc)
+    $(echo "" >> ~/.bashrc) 
+fi;
+
 echo "Current Git global configuration:"
 echo "$(git config --global -l)"
 echo ""
 echo "If you want to edit the configuration, please run:"
 echo "  $new_editor ~/.gitconfig"
 echo ""
-echo "And if you want to edit the .gitignore_global file, please run:"
+echo "If you want to edit the .gitignore_global file, please run:"
 echo "  $new_editor ~/.gitignore_global"
+echo ""
+echo "And if you need to reload your .bashrc file, please run:"
+echo "  source ~/.bashrc"
 echo ""
