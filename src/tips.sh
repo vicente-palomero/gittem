e740d6978e3cf32898607d7659133d20bc22cbd3
luis palomero <luis@declarando.es>
2020-06-25 16:26:45 +0200

    Add changelog-related tips and some minor colors

--- src/tips.sh
+++ src/tips.sh
@@ -10,6 +10,7 @@ usage() {
   echo "  branch    Show tips related with branching"
   echo "  crud      Show how create, read, update or delete files"
   echo "  stage     Show tips related with staging and unstaging files"
+  echo "  changelog Show tips related with changelog events"
   echo ""
   echo "Usage"
   echo "-----"
@@ -20,7 +21,7 @@ usage() {
 }
 
 branch() {
-  echo "Branching tips"
+  echo -e "\033[1mBranching\033[0m tips"
   echo "=============="
   echo "Create a new branch:        git checkout -b <branch_name>"
   echo "Rename a branch:            git branch -m <branch_name>"
@@ -29,7 +30,7 @@ branch() {
 }
 
 crud() {
-  echo "Create, read, update, delete tips"
+  echo -e "\033[1mCreate, read, update, delete\033[0m tips"
   echo "================================="
   echo "Rename a file:              git mv <prev_name> <new_name>"
   echo "Delete a file:              git rm <file>"
@@ -37,7 +38,7 @@ crud() {
 }
 
 stage() {
-  echo "Stage tips"
+  echo -e "\033[1mStage\033[0m tips"
   echo "=========="
   echo "Stage file:                 git add <file>"
   echo "Unstage file:               git reset HEAD <file>"
@@ -47,10 +48,34 @@ stage() {
   echo ""
 }
 
+changelog() {
+  echo -e "\033[1mChangelog-related\033[0m tips (https://keepachangelog.com/en/1.1.0/)"
+  echo "=========="
+  echo "Guiding principles:"
+  echo "-  Changelogs are for humans, not machines."
+  echo "-  There should be an entry for every single version."
+  echo "-  The same types of changes should be grouped."
+  echo "-  Versions and sections should be linkable."
+  echo "-  The latest version comes first."
+  echo "-  The release date of each version is displayed."
+  echo "-  Mention whether you follow Semantic Versioning."
+  echo "=========="
+  echo -e "Types of changes"
+  echo -e "-  \033[4mAdded\033[0m for new features."
+  echo -e "-  \033[4mChanged\033[0m for changes in existing functionality."
+  echo -e "-  \033[4mDeprecated\033[0m for soon-to-be removed features."
+  echo -e "-  \033[4mRemoved\033[0m for now removed features."
+  echo -e "-  \033[4mFixed\033[0m for any bug fixes."
+  echo -e "-  \033[4mSecurity\033[0m in case of vulnerabilities."
+  echo ""
+}
+
+
 all() {
   branch
   crud
   stage
+  changelog
 }
 
 while getopts i:a o; do
@@ -71,6 +96,7 @@ case $which in
 "branch") branch ;;
 "crud") crud;;
 "stage") stage ;;
+"changelog") changelog ;;
 "all") all ;;
 *) echo "Topic $which is not supported." ;;
 
